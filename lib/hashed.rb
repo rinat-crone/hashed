488548a7486aabab8f32358784a7e398609c974f
Rinat Shaykhutdinov <rinat.crone@gmail.com>
2011-12-01 15:28:48 +0400

    Default field bugfix.

--- lib/hashed.rb
+++ lib/hashed.rb
@@ -3,8 +3,9 @@ require "active_record"
 
 module Hashed
   def hashed(opts = { field: :id, only_field: false })
-    opts = { field: opts } if opts.is_a? Symbol
-    result = {}
+    opts          = { field: opts } if opts.is_a? Symbol
+    opts[:field]  = :id             if opts[:field].nil?
+    result        = {}
 
     self.all.each do |row|
       result[row.send(opts[:field])] = (opts[:only_field] ? row.send(opts[:only_field]) : row)
